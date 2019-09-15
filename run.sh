cc8e617384351ea4ab0d2ff0474f4eb43ee672a3
CapBarbossa <47692079+CapBarbossa@users.noreply.github.com>
2019-09-15 12:19:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,6 @@ yum update &&
   yum install docker-ce-19.03.2 docker-ce-cli-19.03.2 containerd.io &&
    curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
     chmod +x /usr/local/bin/docker-compose && 
-    yum -y install wget &&
-    wget http://github.com/CapBarbossa/create_new_server/docker-compose.yaml &&
     docker-compose up -d
     
 
