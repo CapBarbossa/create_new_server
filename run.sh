4e7c5d48f04ca7574745aee1b6930ab64190c90a
CapBarbossa <47692079+CapBarbossa@users.noreply.github.com>
2019-09-15 12:17:46 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+yum update &&
+  yum -y groupinstall "Development Tools" &&
+  yum install -y yum-utils   device-mapper-persistent-data   lvm2 &&
+  yum-config-manager     --add-repo     https://download.docker.com/linux/centos/docker-ce.repo &&
+  yum install docker-ce-19.03.2 docker-ce-cli-19.03.2 containerd.io &&
+   curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+    chmod +x /usr/local/bin/docker-compose && 
+    yum -y install wget &&
+    wget http://github.com/CapBarbossa/create_new_server/docker-compose.yaml &&
+    docker-compose up -d
+    
+
+
+
+
